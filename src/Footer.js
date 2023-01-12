04a1a2129cb602fda8fefee46643035aec2d1d81
nimaaskari <nimaa.dev@gmail.com>
2023-01-12 13:27:52 +0000

    create base project and added header and footer

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.style.css';
+
+function Footer() {
+  return (
+    <div className="footer">
+      <p>All right reserved</p>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.style.css';
+
+function Header() {
+  return (
+    <div className="header">
+      <p>React Shopping Cart</p>
+    </div>
+  );
+}
+
+export default Header;
